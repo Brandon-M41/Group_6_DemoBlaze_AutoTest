16ffcf66421d77e1d6fd2626c445065342227127
Foreign Kenzoku <73410083+ForeignKenzoku@users.noreply.github.com>
2026-04-20 19:21:01 -0400

    my work

---
+++ Scripts/TC_Reg_001/Script1776719578352.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('https://www.demoblaze.com')
+
+WebUI.click(findTestObject('Page_STORE/a_Sign up'))
+
+WebUI.setText(findTestObject('Page_STORE/input_Username_sign-username'), '3y5iohjgwrbsn')
+
+WebUI.setText(findTestObject('Page_STORE/input_Password_sign-password'), 'FatDaniel')
+
+WebUI.click(findTestObject('Page_STORE/button_Sign up'))
+
+WebUI.waitForAlert(5)
+
+alertMessage = WebUI.getAlertText(FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyMatch(alertMessage, '.*Sign up successful.*', true)
+
+WebUI.acceptAlert()
+
+WebUI.closeBrowser()
+
